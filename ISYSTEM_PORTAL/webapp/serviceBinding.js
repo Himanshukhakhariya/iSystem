fe6873d78723bb50be834ec0cc301e33e8d262d5
Himanshukhakhariya <47914899+Himanshukhakhariya@users.noreply.github.com>
2019-06-06 11:27:16 +0530

    Delete serviceBinding.js

--- ISYSTEM_PORTAL/webapp/serviceBinding.js
+++
@@ -1,5 +0,0 @@
-function initModel() {
-	var sUrl = "/ISYSTEM/jezz_Portal/webservice/WebService.xsodata/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-	sap.ui.getCore().setModel(oModel);
-}
